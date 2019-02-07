97262ed73f35571cad60c73dec44b897a13796c2
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-02-07 18:43:45 +0000

    Update learned_player.py

--- Technical_Project/N_Mens_Morris/learned_player.py
+++ Technical_Project/N_Mens_Morris/learned_player.py
@@ -219,9 +219,9 @@ class Learned_Player(object):
 #		self.x_bin = [self.x_empty,self.x_p1,self.x_p2,self.x_game_type,self.x_decision_type]
 		self.ttemp = [self.x_empty,self.x_p1,self.x_p2]
 		self.tempp = [self.x_game_type,self.x_decision_type]
-		self.tttemp = tf.reshape(self.ttemp, shape=[1, 72])
-		self.temppp = tf.reshape(self.tempp, shape=[1, 8])
-		self.x_bin = tf.concat(self.ttemp, self.tempp)
+		self.tttemp = tf.reshape(self.ttemp, shape=[72])
+		self.temppp = tf.reshape(self.tempp, shape=[8])
+		self.x_bin = tf.concat(self.tttemp, self.temppp)
 		self.x = tf.reshape(self.x_bin, shape=[1,self.n_input])
 		self.reward = tf.placeholder(tf.float32,[self.n_classes])
 		self.y = tf.reshape(self.reward, [1, self.n_classes])
