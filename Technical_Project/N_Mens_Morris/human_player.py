d4ef5118d3e9c17251b98429baafebbd08b91f6e
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-02-08 15:41:40 +0000

    Update human_player.py

--- Technical_Project/N_Mens_Morris/human_player.py
+++ Technical_Project/N_Mens_Morris/human_player.py
@@ -1,6 +1,5 @@
 #Packages
 import numpy as np
-import pandas as pd
 import random
 from copy import deepcopy
 import csv
@@ -8,7 +7,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from math import log
-import networkx as nx
 
 
 class Human_Player(object):
