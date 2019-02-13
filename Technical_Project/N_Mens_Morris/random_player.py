b0fe9eabcd456ec5539bd39818f9bdf1aa971dcc
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-02-13 14:17:10 +0000

    Update random_player.py

--- Technical_Project/N_Mens_Morris/random_player.py
+++ Technical_Project/N_Mens_Morris/random_player.py
@@ -1,7 +1,9 @@
 import random
 import numpy as np
 
-adj_dict_3 = {
+adj_dict_3 = [[1,3,4],[0, 2, 4],[1, 4, 5],[0, 4, 6],[0, 1, 2, 3, 5, 6, 7, 8],[2, 4, 8],[7, 3, 4],[6, 8, 4],[7, 4, 5]]
+
+badj_dict_3 = {
 "0": [1, 3, 4],
 "1": [0, 2, 4],
 "2": [1, 4, 5],
@@ -13,7 +15,10 @@ adj_dict_3 = {
 "8": [7, 4, 5],
 }
 
-adj_dict_6 = {
+adj_dict_6 = [[1, 6],[0, 2, 4],[1, 9],[4, 7],[1, 3, 5],[4, 8],[7, 0, 13],[10, 3, 6],[12, 5, 9],[2, 8, 15],[7, 11],[12, 14, 10],[8, 11],
+	     [14, 6],[13, 15, 11],[9, 14]]
+
+badj_dict_6 = {
 "0": [1, 6],
 "1": [0, 2, 4],
 "2": [1, 9],
@@ -32,7 +37,11 @@ adj_dict_6 = {
 "15": [9, 14]
 }
 
-adj_dict_9 = {
+adj_dict_9 = [[1, 9],[0, 2, 4],[1, 14],[4, 10],[1, 3, 5, 7],[4, 13],[7, 11],[4, 6, 8],[12, 7],[0, 21, 10],[11, 18, 3, 9],[6, 15, 10],
+	      [8, 17, 13, 14],[14, 20, 5],[2, 23, 13],[11, 15],[15, 17, 19],[12, 16],[10, 19],[18, 20, 10, 22],[19, 13],[9, 22],
+	      [21, 23, 19],[22, 14]]
+
+badj_dict_9 = {
 "0": [1, 9],
 "1": [0, 2, 4],
 "2": [1, 14],
@@ -59,8 +68,11 @@ adj_dict_9 = {
 "23": [22, 14]
 }
 
+adj_dict_12 = [[1, 9, 3],[0, 2, 4],[1, 14, 5],[4, 10, 0, 6],[1, 3, 5, 7],[4, 13, 2, 8],[7, 11, 3],[4, 6, 8],[12, 7, 5],[0, 21, 10],
+	       [11, 18, 3],[6, 15],[8, 17],[14, 20, 5],[2, 23, 13],[11, 15, 18],[15, 17, 19],[12, 16, 20],[10, 18, 15, 21],
+	       [18, 20, 10, 22],[19, 13, 17, 20],[9, 22, 18],[21, 23, 19],[22, 14, 20]]
 
-adj_dict_12 = {
+badj_dict_12 = {
 "0": [1, 9, 3],
 "1": [0, 2, 4],
 "2": [1, 14, 5],
@@ -100,25 +112,25 @@ class Random_Player(object):
 		valid_moves = []
 		if game_type == 3:
 			for piece in pieces:
-				for space in adj_dict_3[str(piece)]:
+				for space in adj_dict_3[piece]:
 					if space in free_space:
 						valid_moves.append((piece,space))
 
 		if game_type == 6:
 			for piece in pieces:
-				for space in adj_dict_6[str(piece)]:
+				for space in adj_dict_6[piece]:
 					if space in free_space:
 						valid_moves.append((piece,space))
 
 		if game_type == 9:
 			for piece in pieces:
-				for space in adj_dict_9[str(piece)]:
+				for space in adj_dict_9[piece]:
 					if space in free_space:
 						valid_moves.append((piece,space))
 
 		if game_type == 12:
 			for piece in pieces:
-				for space in adj_dict_12[str(piece)]:
+				for space in adj_dict_12[piece]:
 					if space in free_space:
 						valid_moves.append((piece,space))
 
