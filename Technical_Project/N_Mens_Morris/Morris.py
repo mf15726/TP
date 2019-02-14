8cd21c4443fdc0254c7cf6b54482ff8a5d12ca86
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-02-14 18:30:48 +0000

    Update Morris.py

--- Technical_Project/N_Mens_Morris/Morris.py
+++ Technical_Project/N_Mens_Morris/Morris.py
@@ -306,7 +306,7 @@ total_move_no = 100000
 
 human_player = Human_Player()
 random_player = Random_Player()
-learned_player = Learned_Player(epsilon=0, alpha=0.3, gamma=0.9, limit=total_move_no)
+learned_player = Learned_Player(epsilon=0.01, alpha=0.3, gamma=0.9, limit=total_move_no)
 learned_player.sess.run(tf.global_variables_initializer())
 #gameboard = define_board(6)
 #nx.draw(gameboard)
