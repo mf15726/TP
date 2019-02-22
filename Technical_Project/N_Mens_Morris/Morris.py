8fa2c31b9180f04b6f2af00e0689ad070ed8440f
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-02-22 11:49:01 +0000

    Update Morris.py

--- Technical_Project/N_Mens_Morris/Morris.py
+++ Technical_Project/N_Mens_Morris/Morris.py
@@ -355,7 +355,7 @@ def game_play(player1,player2,game_type,print_board,flying,limit):
 
 winner_list = []
 enable_flying = True
-game_type = 9
+game_type = 3
 see_board = False
 total_move_no = 100000
 game_states = [None] * total_move_no
