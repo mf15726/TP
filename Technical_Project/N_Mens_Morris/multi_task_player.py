a24085628b5e93aa54ce0162107747bb6e1a1499
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-02-26 13:56:50 +0000

    Update multi_task_player.py

--- Technical_Project/N_Mens_Morris/multi_task_player.py
+++ Technical_Project/N_Mens_Morris/multi_task_player.py
@@ -201,7 +201,7 @@ class Multi_Task_Player(object):
 		self.n_input_task = self.n_classes_base + 3
 		
 		self.future_steps = 0
-		self.symmetry_index = [None] * self.n_classes
+		self.symmetry_index = [None] * self.n_classes_9
 		self.symmetry_future_index = [None] * self.n_classes
 		self.piece_adj_list = [None] * 12
 		
