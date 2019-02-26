1de851d68d9e999a3186f6fe1b21f395d1cebb84
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-02-26 11:54:12 +0000

    Update Morris.py

--- Technical_Project/N_Mens_Morris/Morris.py
+++ Technical_Project/N_Mens_Morris/Morris.py
@@ -393,7 +393,7 @@ def play_and_learn(total_game_no,multi_task):
 				learned_player.learn(game_type, winner)
 	return winner_list
 		
-winner_list = play_and_learn(10000,multi_task)
+winner_list = play_and_learn(1000,multi_task)
 print('P1 wins = ' + str(winner_list.count(1)))
 print('P2 wins = ' + str(winner_list.count(2)))
 #cProfile.run('play_and_learn(100)')
