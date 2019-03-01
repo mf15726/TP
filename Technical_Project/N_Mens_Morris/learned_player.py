5f671fb4b7da2f7ba1c56d6735879b7cb4383355
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-03-01 13:08:12 +0000

    Update learned_player.py

--- Technical_Project/N_Mens_Morris/learned_player.py
+++ Technical_Project/N_Mens_Morris/learned_player.py
@@ -654,11 +654,14 @@ class Learned_Player(object):
 			sym_list = sym6
 		else:
 			sym_list = sym9
+			
+		print(self.to_index)
+		print(self.from_index)
+		print(self.remove_index)
 		
 		for index, item in enumerate(self.to_index):
 			if None in item:
 				break
-			print(item[0])
 			reward_to = self.reward_function(game_type,winner,item[2],self.to_qval_index[index], decision_type_to, item[0], game_type_input, self.to_future_index[index], item[1])
 			self.sess.run([self.optimiser], feed_dict={self.reward: reward_to, self.input: item[0], self.game_type: game_type_input,
 								   self.decision_type: decision_type_to})
