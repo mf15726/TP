1a6ad4cf111eb702ccb7b6388ddab1532703024c
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-03-01 13:04:12 +0000

    Update learned_player.py

--- Technical_Project/N_Mens_Morris/learned_player.py
+++ Technical_Project/N_Mens_Morris/learned_player.py
@@ -240,8 +240,6 @@ class Learned_Player(object):
 #			kernel_initializer=xavier_initializer
 #		)
 
-		print(self.x)
-
 		l1 = tf.layers.dense(
 			inputs=self.x,
 			units=self.n_input,
@@ -659,6 +657,7 @@ class Learned_Player(object):
 		for index, item in enumerate(self.to_index):
 			if None in item:
 				break
+			print(item[0])
 			reward_to = self.reward_function(game_type,winner,item[2],self.to_qval_index[index], decision_type_to, item[0], game_type_input, self.to_future_index[index], item[1])
 			self.sess.run([self.optimiser], feed_dict={self.reward: reward_to, self.input: item[0], self.game_type: game_type_input,
 								   self.decision_type: decision_type_to})
