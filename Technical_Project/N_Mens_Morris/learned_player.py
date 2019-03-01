1aafa51671cf7d904b83acbc497b8757513916e0
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-03-01 13:20:35 +0000

    Update learned_player.py

--- Technical_Project/N_Mens_Morris/learned_player.py
+++ Technical_Project/N_Mens_Morris/learned_player.py
@@ -403,6 +403,8 @@ class Learned_Player(object):
 			for item in new_state:
 				if item != 0:
 					item = (item % 2) + 1
+		print(state)
+		print(new_state)
 		return new_state
 		
 	def max_next_Q(self, state, game_type_input, player, decision):
