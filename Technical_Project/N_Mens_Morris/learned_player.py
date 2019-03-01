d233a147e55d06b09b0c2c2dd8554d53533d4c41
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-03-01 18:23:24 +0000

    Update learned_player.py

--- Technical_Project/N_Mens_Morris/learned_player.py
+++ Technical_Project/N_Mens_Morris/learned_player.py
@@ -462,8 +462,8 @@ class Learned_Player(object):
 		input_state = self.padding(input_state,game_type)
 		predictions_to = self.sess.run([self.Q_val], feed_dict={self.input: input_state, self.game_type: game_type_input,
 										   self.decision_type: decision_type_to})
-		print(predictions_to[0][0])
-		print(move_no)
+#		print(predictions_to[0][0])
+#		print(move_no)
 		if rand <= 100*self.epsilon:
 			random_move = self.random_move(state, valid_moves, enable_flying, pieces)
 			predictions_from = self.sess.run([self.Q_val], feed_dict={self.input: input_state, self.game_type: game_type_input,
