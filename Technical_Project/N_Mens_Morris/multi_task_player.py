d50fe8bb69c1ed02e56c28489bb7f8c842a8e580
mf15726 <45205952+mf15726@users.noreply.github.com>
2019-03-01 14:51:49 +0000

    Update multi_task_player.py

--- Technical_Project/N_Mens_Morris/multi_task_player.py
+++ Technical_Project/N_Mens_Morris/multi_task_player.py
@@ -792,15 +792,23 @@ class Multi_Task_Player(object):
 		return free_space
 	
 	def reward_function(self, game_type, winner, player, qval_index, decision_type, input_state, task_classes, future_state, move):
-		reward_base = self.sess.run([self.Q_val_base], feed_dict={self.base_input: input_state, self.decision_type: decision_type})
+#		reward_base = self.sess.run([self.Q_val_base], feed_dict={self.base_input: input_state, self.decision_type: decision_type})
 		
-		reward_task = self.task_specific(game_type, decision_type, reward_base)
+#		reward_task = self.task_specific(game_type, decision_type, reward_base)
+
+		reward_base = [0] * self.n_classes_base
+		if game_type = 3:
+			reward_task = [0] * self.n_classes_3
+		elif game_type = 6:
+			reward_task = [0] * self.n_classes_6
+		else:
+			reward_task = [0] * self.n_classes_9
 		if winner == player:
-			reward_base[0][0][move] += 1
-			reward_task[0][0][move] += 1
+			reward_base[move] += 1
+			reward_task[move] += 1
 		if winner == (player % 2) + 1:
-			reward_base[0][0][move] -=  1
-			reward_task[0][0][move] -=  1
+			reward_base[move] -=  1
+			reward_task[move] -=  1
 			
 		for item in reward_base:
 			for i in range(self.future_steps):
